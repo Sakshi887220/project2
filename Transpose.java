f78e08aeaa8ef2586de5a9a6efac6d0d9bfb9ba3
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-08-16 13:40:11 +0530

    Add files via upload

---
+++ Transpose.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package transpose;
+
+/**
+ *
+ * @author Sonam
+ */
+public class Transpose {
+
+ // Java Program to find
+// transpose of a matrix
+         static final int N = 4;
+	
+	// This function stores transpose
+	// of A[][] in B[][]
+	static void transpose(int A[][], int B[][])
+	{
+		int i, j;
+		for (i = 0; i < N; i++)
+			for (j = 0; j < N; j++)
+				B[i][j] = A[j][i];
+	}
+	
+	// Driver code
+	public static void main (String[] args)
+	{
+		int A[][] = { {1, 1, 1, 1},
+					{2, 2, 2, 2},
+					{3, 3, 3, 3},
+					{4, 4, 4, 4}};
+	
+		int B[][] = new int[N][N], i, j;
+	
+		transpose(A, B);
+	
+		System.out.print("Result matrix is \n");
+		for (i = 0; i < N; i++)
+		{
+			for (j = 0; j < N; j++)
+			System.out.print(B[i][j] + " ");
+			System.out.print("\n");
+		}
+	}
+}
+
+
+// This code is contributed by Anant Agarwal.
+
